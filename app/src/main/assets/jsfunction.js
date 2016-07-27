da2ef741537905dd6510ff606af6e3a798330a96
KDisEDDY <LJY68835441@163.com>
2016-07-27 19:06:34 +0800

    js交互

---
+++ app/src/main/assets/jsfunction.js
@@ -0,0 +1,6 @@
+
+function Add(var1,var2){
+	var amount = testFunc.add(var1,var2);
+	alert("the amount is " + amount);
+}
+
--- app/src/main/java/project/ljy/functionutils/WebBrowerActivity.java
+++ app/src/main/java/project/ljy/functionutils/WebBrowerActivity.java
@@ -1,7 +1,9 @@
 package project.ljy.functionutils;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -14,9 +16,18 @@ public class WebBrowerActivity extends AppCompatActivity {
         WebView webView = (WebView) findViewById(R.id.webview);
 
         if (webView != null) {
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.loadUrl("file:///android_asset/testf.html.html");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                WebView.setWebContentsDebuggingEnabled(true);
+            }
+            webView.loadUrl("file:///android_asset/testf.html");
+            webView.setWebChromeClient(new WebChromeClient());
             webView.addJavascriptInterface(new TestFunction(),"testFunc");
+            // 设置支持javascript
+            webView.getSettings().setJavaScriptEnabled(true);
+//启动缓存
+            webView.getSettings().setAppCacheEnabled(true);
+            //设置缓存模式
+            webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         }
     }
 }
